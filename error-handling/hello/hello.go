ab7cfe1efb29a5630320aacce07c940e5c5499e5
Aravind I M <aravindim@yahoo.com>
2023-08-20 21:29:09 +0530

    feat: error handling

---
+++ error-handling/hello/hello.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"example.com/greetings"
+	"fmt"
+	"log"
+)
+
+func main() {
+	// Set properties of logger
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message
+	message, err := greetings.Hello("")
+
+	// Print error into log on error
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print the greeting if no error
+	fmt.Println(message)
+}
