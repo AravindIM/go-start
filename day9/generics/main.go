e6bbccd5fb4ecdf28e2928f5af24d0443772822b
Aravind I M <aravindim@yahoo.com>
2023-08-21 21:28:37 +0530

    feat: generics and interfaces

---
+++ day9/generics/main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+type Number interface {
+	int64 | float64
+}
+
+func SumInts(m map[string]int64) int64 {
+	var s int64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func SumFloats(m map[string]float64) float64 {
+	var s float64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func main() {
+	ints := map[string]int64{
+		"first":  69,
+		"second": 420,
+	}
+
+	floats := map[string]float64{
+		"first":  69.420,
+		"second": 420.69,
+	}
+
+	fmt.Printf("Non-Generic Sums: %v and %v\n",
+		SumInts(ints),
+		SumFloats(floats))
+
+	fmt.Printf("Generic Sums: %v and %v\n",
+		SumNumbers(ints),
+		SumNumbers(floats))
+}
